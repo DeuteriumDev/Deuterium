08b53ba1918640c1defa3dfe6a53ba814033c91c
Ben Vincent <benjc.vincent@gmail.com>
2023-11-01 11:45:42 -0600

    Add pg_mod build script

---
+++ packages/pg_mod/README.md
@@ -0,0 +1,5 @@
+# Deuterium Mods
+
+This package contains the sql migrations for Deuterium data protections. It extends a pre-existing postgres database with ABAC (Attribute Based Access Control). It handles multiple frameworks by templating with jinja2 and formats the distributions with sql-formatter.
+
+Check the builds directory for desired migration.
---
+++ packages/pg_mod/scripts/compile_sql.sh
@@ -0,0 +1,38 @@
+
+#!/usr/bin/env bash
+
+set -o errexit
+set -o nounset
+set -o pipefail
+if [[ "${TRACE-0}" == "1" ]]; then
+    set -o xtrace
+fi
+
+if [[ "${1-}" =~ ^-*h(elp)?$ ]]; then
+    echo 'Usage: ./compile_sql.sh <sql file> <data file>
+
+Template sql w/ [jinja](), and format w/ [sql-formatter]() given file with provided data
+
+'
+    exit
+fi
+
+
+if [[ $# -gt 2 ]]; then
+    echo 'Too many arguments' >&2
+    exit 2
+fi
+
+if [[ $# -lt 2 ]]; then
+    echo 'Too few arguments' >&2
+    exit 2
+fi
+
+# run command relative to called directory, not script directory
+# cd "$(dirname "$1")"
+
+main() {
+  j2 $1 $2 | npx sql-formatter --config ./.sqlformaterrc
+}
+
+main "$@"
