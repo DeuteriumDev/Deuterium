54f9a9a441481b139f0eb6b9122a2db16527ba90
Ben Vincent <benjc.vincent@gmail.com>
2023-11-03 11:04:38 -0600

    feat(#3): Update helper dir

--- packages/pg_mod/tests/unit/functions.spec.js
+++ packages/pg_mod/tests/unit/functions.spec.js
@@ -18,7 +18,7 @@ Steps:
 describe('functions.sql', () => {
   beforeAll(async () => {
     await client.connect();
-    await fileLoader('templates/core/cleanup.sql', 'fixtures/basic.json');
+    await fileLoader('templates/extra/cleanup.sql', 'fixtures/basic.json');
     await fileLoader('templates/core/roles.sql', 'fixtures/basic.json');
     await fileLoader('templates/core/schemas.sql', 'fixtures/basic.json');
     await fileLoader('templates/core/tables.sql', 'fixtures/basic.json');
