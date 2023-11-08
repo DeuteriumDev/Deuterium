14214601e0bf4c7a7a8e6c9d27e48f2522726dc4
Ben Vincent <benjc.vincent@gmail.com>
2023-11-08 14:10:44 -0700

    feat(#4): add framework templates

---
+++ packages/pg_mod/templates/frameworks/postgres.sql
@@ -0,0 +1,6 @@
+{% include 'templates/core/roles.sql' %}
+{% include 'templates/core/schemas.sql' %}
+{% include 'templates/core/tables.sql' %}
+{% include 'templates/core/functions.sql' %}
+{% include 'templates/core/views.sql' %}
+{% include 'templates/core/policies.sql' %}
--- packages/pg_mod/templates/frameworks/supabase.sql
+++ packages/pg_mod/templates/frameworks/supabase.sql
@@ -1,4 +1,13 @@
-{% include '../core/functions.sql' %}
-{% include '../core/tables.sql' %}
-{% include '../core/views.sql' %}
-{% include '../core/policies.sql' %}
+{% include 'templates/core/schemas.sql' %}
+{% include 'templates/core/tables.sql' %}
+{% include 'templates/core/functions.sql' %}
+
+-- supabase
+create or replace function private.get_user_id() returns uuid as $$
+  select auth.uid()
+$$ language sql stable;
+
+comment on function private.get_user_id () is '**SUPABASE** Returns framework user id.';
+
+{% include 'templates/core/views.sql' %}
+{% include 'templates/core/policies.sql' %}
