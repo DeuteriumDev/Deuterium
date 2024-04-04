a54ebcc3c998be11c6f7c4f08994005150c77cdd
Ben Vincent <benjc.vincent@gmail.com>
2024-04-03 17:01:03 -0700

    feat(#20): Update table skeleton

--- services/nextjs_admin_dashboard/src/components/TableSkeleton.tsx
+++ services/nextjs_admin_dashboard/src/components/TableSkeleton.tsx
@@ -1,22 +1,15 @@
-import React from 'react';
 import Skeleton from '~/components/Skeleton';
 
 export default function TableSkeleton() {
   return (
     <div className="pt-10">
       <div className="flex flex-col">
-        <h2 className="ml-1 text-2xl font-bold tracking-tight">Groups</h2>
-        <Skeleton className="w-24 h-10" />
         <div className="grid grid-cols-1 gap-4">
-          {Array(5)
+          {Array(7)
             .fill(null)
             .map((_v, i) => (
               <Skeleton className="w-full h-10" key={`table-skeleton-${i}`} />
             ))}
-        </div>
-        <div className="flex flex-row justify-end pt-5">
-          <Skeleton className="h-10 w-24" />
-          <Skeleton className="h-10 w-24 ml-2" />
         </div>
       </div>
     </div>
