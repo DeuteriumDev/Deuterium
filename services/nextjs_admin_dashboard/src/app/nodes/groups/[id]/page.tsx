e014bc87d08f806785aeb41b333dc99d565b2b4e
Ben Vincent <benjc.vincent@gmail.com>
2024-04-03 17:03:04 -0700

    feat(#20): Add groups page

--- services/nextjs_admin_dashboard/src/app/nodes/groups/[id]/page.tsx
+++ services/nextjs_admin_dashboard/src/app/nodes/groups/[id]/page.tsx
@@ -1,5 +1,40 @@
-import React from 'react';
+import { Suspense } from 'react';
 
-export default function GroupPage() {
-  return <div>GroupPage</div>;
+import GroupsBreadCrumb from '~/components/GroupsBreadCrumb';
+import GroupsForm from '~/components/GroupsForm';
+import GroupsMembersList from '~/components/GroupsMembersList';
+
+import { queryGroup, queryGroupChildren, updateGroup } from './actions';
+import FormSkeleton from '~/components/FormSkeleton';
+
+interface GroupPageProps {
+  params: {
+    id: string;
+  };
+}
+
+export default async function GroupPage(props: GroupPageProps) {
+  const {
+    params: { id },
+  } = props;
+
+  const queryGroupResult = await queryGroup({ id });
+  const queryGroupChildrenResult = await queryGroupChildren({ id });
+
+  return (
+    <div>
+      <h2>Group</h2>
+      <GroupsBreadCrumb id={id} />
+      <div className="grid gap-8 grid-cols-2">
+        <Suspense fallback={<FormSkeleton />}>
+          <GroupsForm
+            group={queryGroupResult.data.rows[0]}
+            groupParents={queryGroupChildrenResult.data.rows}
+            submit={updateGroup}
+          />
+        </Suspense>
+        <GroupsMembersList parent_id={id} />
+      </div>
+    </div>
+  );
 }
